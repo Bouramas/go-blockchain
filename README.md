a90dd98f9c0de20a328eb053d51ac49e40e914a2
ybouramas <ybouramas@omilia.com>
2023-11-30 12:45:53 +0200

    feat: stage 3 completed

---
+++ README.md
@@ -0,0 +1,15 @@
+#Golang - Blockchain
+
+This is a sample blockchain built in Golang.
+
+
+
+
+
+
+
+
+
+go run main.go print
+
+go run main.go add -block "first block"
--- blockchain/block.go
+++ blockchain/block.go
@@ -1,8 +1,10 @@
 package blockchain
 
-type BlockChain struct {
-	Blocks []*Block
-}
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
 
 type Block struct {
 	Hash     []byte
@@ -27,11 +29,28 @@ func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
 }
 
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
+func (b *Block) Serialize() []byte {
+	var res bytes.Buffer
+	encoder := gob.NewEncoder(&res)
+
+	err := encoder.Encode(b)
+	Handle(err)
+
+	return res.Bytes()
 }
 
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	chain.Blocks = append(chain.Blocks, CreateBlock(data, prevBlock.Hash))
+func Deserialize(data []byte) *Block {
+	var block Block
+	decoder := gob.NewDecoder(bytes.NewBuffer(data))
+
+	err := decoder.Decode(&block)
+	Handle(err)
+
+	return &block
+}
+
+func Handle(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
